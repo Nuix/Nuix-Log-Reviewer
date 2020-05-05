5697da7872f1a2a32313ed5a1da92469ac15888d
Jason Wells <jason.wells@nuix.com>
2020-05-05 15:04:59 -0700

    Create MultiLineClassifier.cs
    Added classifier which classifies log entries which span multiple lines

---
+++ Visual Studio/NuixLogReviewer/LogRepository/Classifiers/MultiLineClassifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuixLogReviewer.LogRepository.Classifiers
+{
+    public class MultiLineClassifier : IEntryClassifier
+    {
+        public IEnumerable<string> Classify(NuixLogEntry entry)
+        {
+            if (entry.Content.Contains("\n") || entry.Content.Contains("\r"))
+            {
+                return new string[] { "multi_line" };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
